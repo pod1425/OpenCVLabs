98c259839c51a137bf9f4d1a4a73fd81e141a4ed
StreakerOne <i.o.tkachov@student.khai.edu>
2022-02-13 16:46:02 +0200

    OpenCV lib added

---
+++ src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.opencv.core.Core;
+
+public class test{
+
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Welcome to OpenCV version " + Core.VERSION);
+
+    }
+}
