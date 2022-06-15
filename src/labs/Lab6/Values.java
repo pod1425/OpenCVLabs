78abb399c6c10fd121b307b15b67aa995f8d4aad
Streaker1425 <i.o.tkachov@student.khai.edu>
2022-06-15 14:26:07 +0300

    Homework v1

--- src/labs/Lab6/Values.java
+++
@@ -1,9 +0,0 @@
-package labs.Lab6;
-
-public class Values {
-    public static final String H_WIN_NAME = "Hue_channel";
-    public static final String S_WIN_NAME = "Saturation_channel";
-    public static final String V_WIN_NAME = "Value_channel";
-
-    public static final String SRC_PATH = "src\\labs\\Lab6\\source\\";
-}
